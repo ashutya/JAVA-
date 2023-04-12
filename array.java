1dc21be35f006df197d3d7c1a55d3eecf6c80812
Ashu Tyagi <118053256+ashutya@users.noreply.github.com>
2023-04-12 21:07:27 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,15 @@
+public class array {
+    public static void main(String args[])
+    {
+        
+        int[] marks = new int[10];
+        marks[0]=50;    
+        marks[1]=56;
+       
+        for(int i=0;i<2;i++)
+        {
+            System.out.println(marks[i]);
+        }
+    }
+    
+}
