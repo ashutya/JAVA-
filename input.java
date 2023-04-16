9bd05ad5aa8175a8081415f76ca4a1531852d2f4
Ashu Tyagi <118053256+ashutya@users.noreply.github.com>
2023-04-16 14:13:46 +0530

    Add files via upload

---
+++ input.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class input 
+{
+    public static void main(String[] args)
+    {
+        Scanner Sc = new  Scanner(System.in);
+                                          
+         int a=Sc.nextInt();
+         int b=Sc.nextInt();
+         int sum=a+b;
+         System.out.println("sum="+sum);
+        Sc.close();
+    }    
+
+}
