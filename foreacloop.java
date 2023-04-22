d85ec2ae636a7a1b2fb55a5c1130173882cfd528
Ashu Tyagi <118053256+ashutya@users.noreply.github.com>
2023-04-22 15:26:24 +0530

    Add files via upload

---
+++ foreacloop.java
@@ -0,0 +1,24 @@
+package ashuproject;
+
+import java.util.Scanner;
+
+public class foreacloop {
+
+	public static void main(String[] args) {
+		int a;
+		Scanner sc = new Scanner(System.in);
+		{
+			System.out.println("enter 10 numbers");
+			for(int i=0;i<10;i++)
+		{
+			a=sc.nextInt();
+		}
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
+}
