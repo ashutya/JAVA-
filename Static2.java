9726f8f1eed59ff85699deec99abbfae6d7a5858
Ashu Tyagi <118053256+ashutya@users.noreply.github.com>
2023-05-06 22:59:06 +0530

    Add files via upload

---
+++ Static2.java
@@ -0,0 +1,18 @@
+package ashuproject;
+
+public class Static2 {
+
+	public static void main(String[] args) {
+		
+       Abc.display();
+	}
+
+}
+class Abc
+{
+   static void display()
+	{
+		System.out.println("hello thi is static method");
+	}
+	
+}
