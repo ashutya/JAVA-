78e8095bc705f8ef2108c93464b10ecd08296a17
Ashu Tyagi <118053256+ashutya@users.noreply.github.com>
2023-05-07 13:55:54 +0530

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,14 @@
+package ashuproject;
+
+public class substring {
+
+	public static void main(String[] args) {
+		String name = "ashutyagi";
+		String name2 = name.substring(0, 4);
+		System.out.println(name);
+		System.out.println(name2);
+			
+
+	}
+
+}
