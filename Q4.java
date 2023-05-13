977e40be255d28328f4996f26ef1a5a852a7cf2d
Ashu Tyagi <118053256+ashutya@users.noreply.github.com>
2023-05-13 15:37:42 +0530

    Add files via upload

---
+++ Q4.java
@@ -0,0 +1,16 @@
+package ashuproject;
+
+ class Simpleobject
+{
+ 	  Simpleobject()
+ 	  {
+ 		  System.out.println("this is simple class");
+ 	  }
+}
+public class Q4
+{
+	public static void main(String[] args) 
+	{
+      Simpleobject b = new Simpleobject();
+	}
+}
